df7ac5202bd623caf7b53c1dc136d076e6df054b
siddalinga patil <siddubpolicepatil@gmail.com>
2023-06-06 15:19:08 +0530

    first commit

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,10 @@
+import os
+import sys
+from src.logger import logging
+from src.exception import CustomException
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+
+##initalize the data ingestion configaration
